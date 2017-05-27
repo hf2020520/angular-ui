82907560b9afd676c82efaa3450d7b400b55d5e3
tanbo <464328895@qq.com>
2017-05-27 14:01:53 +0800

    生产环境配置

---
+++ server/product-server.js
@@ -0,0 +1,24 @@
+const historyApiFallback = require("connect-history-api-fallback");
+const favicon = require('express-favicon');
+const ip = require('ip');
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const appPort = '1234';
+const appBasePath = path.resolve(__dirname, '../dist/');
+
+
+app.use(historyApiFallback({}));
+
+app.use(favicon('/favicon.ico'));
+
+app.use(express.static(appBasePath));
+
+app.listen(appPort, ip.address(), error => {
+    if (error) {
+        console.log(error);
+        return;
+    }
+    console.log('http://' + ip.address() + ':' + appPort);
+});
