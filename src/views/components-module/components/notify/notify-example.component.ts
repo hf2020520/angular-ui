215d6f85f9326bf023a279968a2039bfc601e079
tanbo <464328895@qq.com>
2017-06-03 14:02:25 +0800

    样式更改

--- src/views/components-module/components/notify/notify-example.component.ts
+++ src/views/components-module/components/notify/notify-example.component.ts
@@ -12,7 +12,7 @@ export class NotifyExampleComponent {
         let config: NotifyConfig = {
             content: '你提供的消息',
             autoHide: true, // 可选，是否自动隐藏
-            type: NotifyType.Info, // 可选，显示风格
+            type: NotifyType.Primary, // 可选，显示风格
             time: 5000 // 可选，停留时间，如果autoHide为false，则此项不生效
         };
         this.notifyService.push(config);
